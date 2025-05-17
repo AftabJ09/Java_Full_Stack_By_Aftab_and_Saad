f15f87fb6183d1c99acb8433c29915fd957afaca
AftabJ09 <aftab.50100@gmail.com>
2025-05-18 00:01:29 +0530

    updated file

--- Operators.java
+++ Operators.java
@@ -1,6 +1,7 @@
 public class Operators {
     public static void main(String[] Args){
         System.out.println("These are the operators");
+        System.out.println("Thank you Guys");
     }
 }
 
