062a3f5b44ed1ecf5232df30ee3039fc3d276257
AftabJ09 <aftab.50100@gmail.com>
2025-05-18 18:32:22 +0530

    operators

--- Operators.java
+++ Operators.java
@@ -1,8 +1,38 @@
+import java.util.Scanner;
 public class Operators {
     public static void main(String[] Args){
-        System.out.println("These are the operators");
-        System.out.println("Thank you Guys");
-    }
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter A value: ");
+            int a = sc.nextInt();
+            System.out.println("Enter B value: ");
+            int b = sc.nextInt();
+
+            /* Relational operators */
+            System.out.println("The answer is: "+(a>b));
+            System.out.println("The answer is: "+(a<b));
+            System.out.println("The answer is: "+(a>=b));
+            System.out.println("The answer is: "+(a==b));
+            System.out.println("The answer is: "+(a!=b));
+
+            /*Logical Operators*/
+            System.out.println("The AND Answer is: "+(a>b && b<=a));
+            System.out.println("The OR Answer is: "+(a>b || b<=a));
+            System.out.println("The NOT Answer is: "+(!(a>b && b<=a)));
+
+            /*Bitwise operator */
+            System.out.println("The BITWISE AND Answer is: "+(a&b));
+            System.out.println("The BITWISE OR Answer is: "+(a|b));
+            System.out.println("The BITWISE XOR Answer is: "+(a^b));
+            System.out.println("The COMPLIMENT is: "+(~a));
+            System.out.println("The LEFT SHIFT is: "+(a<<2));
+            System.out.println("The RIGHT SHIFT is: "+(a>>2));
+
+            /* Assignment Operators */
+            System.out.println("The Add and assignment: "+(a += 1));
+            System.out.println("The Minus and assignment: "+(a -= 1));
+            System.out.println("The Multiply and assignment: "+( a *= 2));
+
+        }
 }
 
 
