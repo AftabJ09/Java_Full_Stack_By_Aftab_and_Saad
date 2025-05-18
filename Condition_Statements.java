769b64909bbdc3dce57f0344d3643b160562df29
AftabJ09 <aftab.50100@gmail.com>
2025-05-18 18:35:17 +0530

    Condition Statements

---
+++ Condition_Statements.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Condition_Statements {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter first number: ");
+    int number1 = input.nextInt();
+    System.out.println("Enter second number: ");
+    int number2 = input.nextInt();
+    System.out.println("Enter third number: ");
+    int number3 = input.nextInt();
+    int greater = 0;
+    if(number1 > number2){
+        if(number1 > number3){
+            System.out.println(number1+ " is greater");
+        }else{
+            System.out.println(number3+ " is greater");
+        }
+    }else{
+        if(number2 > number3){
+            System.out.println(number2+" is greater");
+        }else{
+            System.out.println(number3+" is greater");
+        }
+    }
+}
+
+}
