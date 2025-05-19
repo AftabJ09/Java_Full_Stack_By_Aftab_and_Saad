61e026e75e617a77132caf68806e9583315284c4
mansoorsaadmohammed2003 <mansoorsaadmohammed@gmail.com>
2025-05-19 12:30:00 +0530

    testing

--- Ternery.java
+++
@@ -1,8 +0,0 @@
-public class Ternery {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int x = (a>b) ? a:b;
-        System.out.println(x);
-    }
-}
