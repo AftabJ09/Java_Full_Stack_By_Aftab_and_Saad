20329bc90ae7309526213f9d608c0188490f425d
Dreamless07 <saadkhan7903@gmail.com>
2025-05-19 15:07:32 +0530

    Continue program added on 19/05/2025

---
+++ Continueprogram.java
@@ -0,0 +1,15 @@
+public class Continueprogram {
+    public static void main(String[] args) {
+        int x=10;
+        while(x<15)
+        {
+            x++;
+            if(x==12)
+            {
+                continue;
+            }
+            System.out.println(x);
+            System.out.println(" ");
+        }
+    }
+}
