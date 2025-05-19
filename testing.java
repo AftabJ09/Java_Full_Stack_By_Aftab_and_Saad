1737636744826d1f22ef1da52738061cf1f5a9f1
mansoorsaadmohammed2003 <mansoorsaadmohammed@gmail.com>
2025-05-19 15:07:09 +0530

    First code

--- testing.java
+++ testing.java
@@ -1,6 +1,14 @@
 public class testing {
     public static void main(String[] args) {
+    int x=10;
+    while (x < 15){
+        x++;
+        if(x==12){
+            System.out.println(" /][][][][");
+            continue;
+        }
+        System.out.println(x);
 
-        System.out.println("Hello testing");
+    }
     }
 }
