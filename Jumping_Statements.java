de514dd466843505b2ecb06d9498c03c8ebca209
AftabJ09 <aftab.50100@gmail.com>
2025-05-19 15:13:33 +0530

    Class 2: Jumping Statement code (19-MAY-2025)

--- Jumping_Statements.java
+++ Jumping_Statements.java
@@ -3,12 +3,17 @@ public class Jumping_Statements
     public static void main(String[] args) {
         int x = 1;
         while(x < 25){
+            if(x == 2){
+                System.out.print("Skip ");
+                x++;
+                continue;
+            }
+            System.out.print(x);
+            System.out.print(" ");
+            x++;
             if(x == 15){
                 break;
             }
-            System.out.print(x);
-            x++;
-            System.out.print(" ");
 
         }
     }
