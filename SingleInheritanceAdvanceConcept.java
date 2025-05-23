722abf7906637e04e6004261a3d8de74eb1defe2
AftabJ09 <aftab.50100@gmail.com>
2025-05-23 10:30:59 +0530

    Class 5: OPPS ( Single Inheritance with constructor Standard code) (22-MAY-2025)

--- SingleInheritanceAdvanceConcept.java
+++ SingleInheritanceAdvanceConcept.java
@@ -4,10 +4,9 @@ class Vehicle{
     Vehicle(String color,String model){
         this.color = color;
         this.model = model;
-        print_data1();
 
     }
-    void print_data1(){
+    protected void print_data1(){
         System.out.println("\nParent class data: \ncolor: "+color+"\nmodel: "+model);
 
     }
@@ -29,9 +28,11 @@ class Bmw extends Vehicle{
 public class SingleInheritanceAdvanceConcept {
     public static void main(String[] args) {
     Vehicle obj1 = new Vehicle("red","2021");
+    obj1.print_data1();
     Bmw obj2 = new Bmw("black","2023","100 litres");
-
+    obj2.print_data1();
     obj2.print_data2();
+
 
     }
 }
