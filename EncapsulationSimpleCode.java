c78fe7bb3d559e998db00fe95b0fb1c3c64a1dea
AftabJ09 <aftab.50100@gmail.com>
2025-05-27 11:07:47 +0530

    Class 8: OOPS (Encapsulation With getter and setter) (27-MAY-2025)

--- EncapsulationSimpleCode.java
+++ EncapsulationSimpleCode.java
@@ -7,10 +7,20 @@ class EncapsulateUserDetail{
     public int getAge(){
         return  userAge;
     }
+
+    public void SetName(String name){
+        username = name;
+    }
+    public void SetAge(int age){
+        userAge = age;
+    }
 }
 public class EncapsulationSimpleCode {
     public static void main(String[] args) {
         EncapsulateUserDetail IN = new EncapsulateUserDetail();
+        System.out.println("Name: "+IN.getName()+"\nAge: "+IN.getAge());
+        IN.SetName("Saad");
+        IN.SetAge(22);
         System.out.println("Name: "+IN.getName()+"\nAge: "+IN.getAge());
     }
 }
