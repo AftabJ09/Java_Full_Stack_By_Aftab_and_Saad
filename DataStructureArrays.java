a7af00c9391b387474b6feb346677fc945259bd1
AftabJ09 <aftab.50100@gmail.com>
2025-06-02 10:56:56 +0530

    Class 11: Data Structure (Array Simple programme) (29-MAY-2025)

---
+++ DataStructureArrays.java
@@ -0,0 +1,9 @@
+public class DataStructureArrays {
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5,6};
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        };
+        if(array.length <3) System.out.println("The work is done");
+    }
+}
