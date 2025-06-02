bc14d4dbb7392cd3229665700f0f57d841d0fa6a
AftabJ09 <aftab.50100@gmail.com>
2025-06-02 11:17:06 +0530

    Class 11: Data Structure (Array Instantiation using new key word) (02-JUNE-2025)

---
+++ DataStructureArrayBasic.java
@@ -0,0 +1,14 @@
+public class DataStructureArrayBasic {
+    public static void main(String[] args) {
+       int Numbers[] = new int[5];
+       Numbers[0] = 10;
+       Numbers[1] = 20;
+       Numbers[2] = 30;
+       Numbers[3] = 40;
+       Numbers[4] = 50;
+        for(int i=0;i<Numbers.length;i++){
+           System.out.println(Numbers[i]);
+       }
+
+    }
+}
