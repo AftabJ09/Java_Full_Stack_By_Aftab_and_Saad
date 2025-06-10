1279659c6ec9ba7f9ad737f280dde2229ed3d725
AftabJ09 <aftab.50100@gmail.com>
2025-06-10 10:30:05 +0530

    Class 13: File handling (File creation) (09-JUNE-2025)

--- File_Operation/src/File_management_create.java
+++ File_Operation/src/File_management_create.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileOutputStream;
+
 public class File_management_create {
     public static void main(String[] args) throws Exception{
        String File_name = "Data.txt";
@@ -8,5 +10,9 @@ public class File_management_create {
        }else{
            System.out.println("Failed to create the file");
        }
+        FileOutputStream fos = new FileOutputStream(File_name);
+       String text = file.getName();
+       System.out.println(text);
+       System.out.println(file.getAbsoluteFile());
     }
 }
