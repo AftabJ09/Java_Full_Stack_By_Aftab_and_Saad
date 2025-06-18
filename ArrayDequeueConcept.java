6363e2de797624a5db261aed9615b3fad36002b8
AftabJ09 <aftab.50100@gmail.com>
2025-06-18 11:25:45 +0530

    Class 19: Collection (Array Deque) (18-JUNE-2025)

---
+++ ArrayDequeueConcept.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+
+public class ArrayDequeueConcept {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+        numbers.push(1);
+        System.out.println(numbers);
+        numbers.add(2);
+        System.out.println(numbers);
+        numbers.addLast(3);
+        System.out.println(numbers.peek());
+        System.out.println(numbers);
+        numbers.addFirst(4);
+        System.out.println(numbers);
+        System.out.println(numbers.peek());
+        System.out.println(numbers.removeLast());
+        System.out.println(numbers.peek());
+        System.out.println(numbers);
+        numbers.addLast(5);
+        System.out.println(numbers);
+        numbers.addFirst(2);
+        System.out.println(numbers);
+        numbers.removeFirst();
+        System.out.println(numbers);
+        numbers.removeLast();
+        System.out.println(numbers);
+    }
+}
