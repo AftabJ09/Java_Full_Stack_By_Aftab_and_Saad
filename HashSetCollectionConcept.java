7f3bf87edc136eb385d8059210136d4be6ce94fa
AftabJ09 <aftab.50100@gmail.com>
2025-06-19 10:39:31 +0530

    Class 20: Collection (HashSet) (19-JUNE-2025)

--- HashSetCollectionConcept.java
+++ HashSetCollectionConcept.java
@@ -1,14 +1,13 @@
 import java.util.HashSet;
 import java.util.Set;
-public class HashSetCollectionConcept {
-    public static void main(String[] args) {
+public class HashSetCollectionConcept{
+    public static void main(String[] args){
         Set<Integer> set = new HashSet<>();
         set.add(1);
-        set.add(2);
-        set.add(3);
-        set.add(4);
-        set.add(5);
         System.out.println(set);
+        set.add(3);
+        System.out.println(set);
+        set.add(7);
         System.out.println(set);
 
     }
