e52a8a9ef56360539bba5d337332a4228d167c23
AftabJ09 <aftab.50100@gmail.com>
2025-06-20 10:47:38 +0530

    Class 21: Java Feature (20-JUNE-2025)

---
+++ Feature1.java
@@ -0,0 +1,16 @@
+interface  GEM{
+    default void demo(){
+        System.out.println("You got a Gem!");
+    }
+
+}
+
+class Gem implements GEM{
+
+}
+public class Feature1 {
+    public static void main(String[] args) {
+        Gem gem = new Gem();
+        gem.demo();
+    }
+}
