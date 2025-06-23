48082e57321b4bcf254c7d19ffd481a051943e6d
AftabJ09 <aftab.50100@gmail.com>
2025-06-23 10:23:42 +0530

    Class 21: Java Feature (20-JUNE-2025)

---
+++ Feature6.java
@@ -0,0 +1,18 @@
+interface Operation{
+    int operator(int a,int b);
+}
+class DoesOperation implements Operation{
+    public int add_operation(int a,int b){
+        return a+b;
+    }
+
+}
+
+public class Feature6 {
+    public static void main(String[] args) {
+        int value = 0;
+        Operation obj = (int a,int b) -> System.out.println(value );
+
+
+    }
+}
