44eaf37c2718ef853f9b79bb6a36c8e4dffd2ef1
AftabJ09 <aftab.50100@gmail.com>
2025-06-24 10:46:11 +0530

    Class 23: Java Feature (24-JUNE-2025)

---
+++ Feature8.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Feature8 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+        System.out.println(list);
+        Predicate<Integer> pred = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer i) {
+                return i%2 == 0;
+            }
+        };
+        list.stream().filter(pred).forEach(System.out::println);
+
+    }
+}
