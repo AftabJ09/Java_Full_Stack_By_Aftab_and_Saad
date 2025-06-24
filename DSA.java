1620a5b5dcf89cad729a161eb1dd4a154a9b4d2d
AftabJ09 <aftab.50100@gmail.com>
2025-06-24 10:29:42 +0530

    Class 22: Java Feature (23-JUNE-2025)

---
+++ DSA.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class DSA {
+    public static void main(String[] args) {
+        HashSet<Integer> Vertices = new HashSet<Integer>();
+        Vertices.add(1);
+        Vertices.add(2);
+        Vertices.add(3);
+        Vertices.add(4);
+        Vertices.add(5);
+        System.out.println(Vertices);
+        List<Integer> Edges = new ArrayList<>();
+        Edges.add(1);
+        Edges.add(2);
+        System.out.println(Edges);
+
+    }
+}
